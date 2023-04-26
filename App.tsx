d0201747aa80ee7c6505b34ac60c3bbede183ac8
vineethpawar <vineethpawar99@gmail.com>
2023-04-27 00:37:30 +0530

    Added React navigation to NB + RN

--- App.tsx
+++ App.tsx
@@ -2,11 +2,22 @@
 import React from 'react';
 import {NativeBaseProvider, Box} from 'native-base';
 import {theme} from './theme';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+  return <Box variant="def">Hello world</Box>;
+};
 export default function App() {
   return (
-    <NativeBaseProvider theme={theme}>
-      <Box variant="def">Hello world</Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider theme={theme}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
